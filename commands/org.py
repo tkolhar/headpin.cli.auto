08618933c2d3b04ff30e6f7ffd7c755ba4b9004c
tazim kolhar <tkolhar@redhat.com>
2012-03-30 17:53:53 +0530

    automation for commands org,user,userrole

---
+++ commands/org.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import subprocess
+#from commands.base import Base
+
+class Org:
+    _command_ = "org"
+    _user_flag_ = "-u"
+    _password_flag_ = "-p"
+
+    def org_create(self,orgname,u="admin",p="admin"):
+        _username_flag_ = "--name"
+        pout = subprocess.Popen(["headpin", self._user_flag_, u, self._password_flag_, p, self._command_, "create", _username_flag_, orgname], stdout=subprocess.PIPE)
+        return pout.communicate()
+    
+    def org_list(self, u="admin", p="admin"):
+        pout = subprocess.Popen(["headpin", self._user_flag_, u, self._password_flag_, p, self._command_, "list"], stdout=subprocess.PIPE)
+        return pout.communicate()
+
+
+    def org_info(self,orgname,u="admin",p="admin"):
+	_name_flag_ = "--name"
+	pout = subprocess.Popen(["headpin", self._user_flag_, u, self._password_flag_, p, self._command_, "info", _name_flag_, orgname], stdout=subprocess.PIPE)
+        return pout.communicate()
+
+    def org_update(self,orgname,u="admin",p="admin"):
+	__name_flag_ = "--name"
+	pout = subprocess.Popen(["headpin", self._user_flag_, u, self._password_flag_, p, self._command_, "update", _name_flag_, orgname], stdout=subprocess.PIPE)
+        return pout.communicate()
+
+
+    def org_delete(self,orgname,u="admin",p="admin"):
+        _name_flag_ = "--name"
+        pout = subprocess.Popen(["headpin", self._user_flag_, u, self._password_flag_, p, self._command_, "delete",_name_flag_, orgname], stdout=subprocess.PIPE)
+        return pout.communicate()
+    
+
+   
+        
+        
+        
+        
