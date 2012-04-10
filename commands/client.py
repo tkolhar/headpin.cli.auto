ee93e5ec8b99e864941130ae3280c7f11476532a
tazim_k <tkolhar@redhat.com>
2012-04-10 03:18:06 -0400

    automation and test for command client

---
+++ commands/client.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import subprocess
+#from commands.base import Base
+
+class Client:
+    _command_ = "client"
+    _user_flag_ = "-u"
+    _password_flag_ = "-p"
+    
+    def client_remember(self, optionname,value,u="admin", p="admin"):
+	_optionname_flag_ ="--option"
+	_valuename_flag_ = "--value"
+        pout = subprocess.Popen(["headpin", self._user_flag_, u, self._password_flag_, p, self._command_, "remember", _optionname_flag_, optionname, _valuename_flag_, value], stdout=subprocess.PIPE)
+        return pout.communicate()
+
+    def client_forget(self,optionname,value,u="admin",p="admin"):
+	_optionname_flag_ = "--option"
+	_valuename_flag_ = "--value"
+	pout = subprocess.Popen(["headpin", self._user_flag_, u, self._password_flag_, p, self._command_, "forget", _optionname_flag_, optionname, _valuename_flag_, value], stdout=subprocess.PIPE)
+        return pout.communicate()
+
+
+    def client_saved_options(self,u="admin",p="admin"):
+        pout = subprocess.Popen(["headpin", self._user_flag_, u, self._password_flag_, p, self._command_, "saved_options"], stdout=subprocess.PIPE)
+        return pout.communicate()
---
+++ tests/test_cli_client.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import pytest
+from unittestzero import Assert
+from commands.client import Client
+from commands.base import Base
+
+xfail = pytest.mark.xfail
+
+class TestClient:
+    
+    def test_client_remember(self):
+        client = Client()
+	base = Base()
+ 	optionname  = "org"
+	value = "Org%s" % base.random_string()
+        out, err = client.client_remember(optionname,value)
+        Assert.equal(err,None)
+
+    def test_client_forget(self):
+        client = Client()
+        base = Base()
+        optionname  = "org"
+        value = "Org%s" % base.random_string()
+        out, err = client.client_forget(optionname,value)
+        Assert.equal(err,None)
+
+    def test_client_saved_options(self):
+        client = Client()
+        out, err = client.client_saved_options()
+        Assert.equal(err,None)
+
